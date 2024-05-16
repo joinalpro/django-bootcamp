06c82bcbbf81e3e1605e2ada76161eec3c6fb143
joinalzx <joinalzx@gmail.com>
2024-05-16 12:36:08 +0600

    if, else, for_loop, break, continue

---
+++ for_loop.py
@@ -0,0 +1,5 @@
+courses = ['Machine Learning', "Django", "Deep Learning", "Big Data"]
+for course in courses: 
+    if course == "Django":
+        continue
+    print("Course Name:", course)
---
+++ if_else.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age > 30:
+    print("Please pay taka", 800 )
+elif age > 6:
+    print("Please pay taka", 600)
+else: 
+    print("Please pay taka", 300)
