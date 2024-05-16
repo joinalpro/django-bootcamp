c32c90f503a6fc7b1c79ec87a3a88436378bf0be
joinalzx <joinalzx@gmail.com>
2024-05-16 12:51:24 +0600

    while loop, function

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,12 @@
-courses = ['Machine Learning', "Django", "Deep Learning", "Big Data"]
-for course in courses: 
-    if course == "Django":
-        continue
-    print("Course Name:", course)
+# courses = ['Machine Learning', "Django", "Deep Learning", "Big Data"]
+# for course in courses: 
+#     if course == "Django":
+#         continue
+#     print("Course Name:", course)
+
+# while loop
+
+counter = 1
+while counter <= 5:
+    print(counter)
+    counter  +=1
---
+++ function.py
@@ -0,0 +1,10 @@
+def bootcamp():
+    print("Welcome to Django Bootcamp")
+
+bootcamp()
+
+def addition (x, y):
+    c = x + y
+    print(c)
+
+addition( 3, 4)
